332938ea02e4289cd6339889257578a6dcbd85b1
Marco Julian Moser <hyper_chrome@gmx.at>
2024-04-08 21:36:32 +0000

    improved MockObjects (iteration, https_method separation), add test for animals.get

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,18 +9,6 @@ from sxapi.integrationV2 import IntegrationAPIV2
 from sxapi.publicV2 import PublicAPIV2
 
 
-class ResponseMock(Response):
-    def __init__(self, value, status_code, exception=None):
-        super().__init__()
-
-        self.exception = exception
-        self.value = value
-        self.status_code = status_code
-
-    def json(self, **kwargs):
-        return self.value
-
-
 class SxMainTestParser(SxApiMainParser):
     def __init__(self, subparsers=True):
         self.config = Config(configfile="./tests/cli_tests/test-config.conf")
@@ -32,49 +20,202 @@ class CliTest(Cli):
     sx_main_parser = SxMainTestParser(subparsers=True)
 
 
-class APIMock:
+class ResponseMock(Response):
+    """
+    Object to mock a response object from the requests library.
+    """
+
+    def __init__(self, value, status_code, exception=None, iterator=False):
+        """
+        Initialize a new response object.
+
+        Args:
+            value (Any): Desired return value of the response object.
+            status_code (int): Desired status code of the response object.
+            exception (Exception): Desired exception to be raised by the response object.
+            iterator (bool): If value is an iterator and mocked call is performed several types
+                set this to True in order to get the next value of the iterator on each call.
+        """
+
+        super().__init__()
+
+        self.value = value
+        self.status_code = status_code
+        self.exception = exception
+        self.iterator = iterator
+
+    def json(self, **kwargs):
+        """
+        Mock the json method of the response object.
+        """
+        return self.value
+
+
+class _APIMock:
+    """
+    Base Mock object for the API classes.
+    """
+
     def __init__(self):
         super().__init__()
 
-        self._return_value = None
-        self.called_with = []
+        self.get_iterator = None
+        self.post_iterator = None
+        self.put_iterator = None
+        self.delete_iterator = None
 
-    def mock_return_value(self, return_value):
+        self.get_return_value = None
+        self.post_return_value = None
+        self.put_return_value = None
+        self.delete_return_value = None
+
+        self.get_called_with = []
+        self.post_called_with = []
+        self.put_called_with = []
+        self.delete_called_with = []
+
+    def mock_get_return_value(self, return_value):
+        """
+        Set the return value of the mock object.
+        Args:
+            return_value:
+
+        Returns:
+
+        """
         if not isinstance(return_value, ResponseMock):
             raise ValueError("return_value is not of type ResponseMock")
 
-        self._return_value = return_value
+        self.get_return_value = return_value
 
-    def reset_mock(self):
-        self._return_value = []
+        if return_value.iterator:
+            self.get_iterator = iter(self.get_return_value.value)
+
+    def mock_post_return_value(self, return_value):
+        """
+        Set the return value of the mock object.
+        Args:
+            return_value:
+
+        Returns:
+
+        """
+        if not isinstance(return_value, ResponseMock):
+            raise ValueError("return_value is not of type ResponseMock")
+
+        self.post_return_value = return_value
+
+        if return_value.iterator:
+            self.post_iterator = iter(self.post_return_value.value)
+
+    def mock_put_return_value(self, return_value):
+        """
+        Set the return value of the mock object.
+        Args:
+            return_value:
+
+        Returns:
+
+        """
+        if not isinstance(return_value, ResponseMock):
+            raise ValueError("return_value is not of type ResponseMock")
+
+        self.put_return_value = return_value
+
+        if return_value.iterator:
+            self.put_iterator = iter(self.put_return_value.value)
+
+    def mock_delete_return_value(self, return_value):
+        """
+        Set the return value of the mock object.
+        Args:
+            return_value:
+
+        Returns:
+
+        """
+        if not isinstance(return_value, ResponseMock):
+            raise ValueError("return_value is not of type ResponseMock")
+
+        self.delete_return_value = return_value
+
+        if return_value.iterator:
+            self.delete_iterator = iter(self.delete_return_value.value)
+
+    def reset_mock(self, get=False, post=False, put=False, delete=False):
+
+        # if nothing is set reset all
+        if not get and not post and not put and not delete:
+            get = post = put = delete = True
+
+        if get:
+            self.get_return_value = []
+            self.get_called_with = []
+        if post:
+            self.post_return_value = []
+            self.post_called_with = []
+        if put:
+            self.put_return_value = []
+            self.put_called_with = []
+        if delete:
+            self.delete_return_value = []
+            self.delete_called_with = []
 
     @check_response
     def get(self, path, *args, **kwargs):
-        self.called_with.append({"path": path, "kwargs": kwargs})
-        return self._return_value
+        self.get_called_with.append({"path": path, "kwargs": kwargs})
+
+        if self.get_return_value.exception:
+            raise self.get_return_value.exception
+
+        if self.get_iterator:
+            self.get_return_value.value = next(self.get_iterator)
+
+        return self.get_return_value
 
     @check_response
     def post(self, path, *args, **kwargs):
-        self.called_with.append({"path": path, "kwargs": kwargs})
-        return self._return_value
+        self.post_called_with.append({"path": path, "kwargs": kwargs})
+
+        if self.post_return_value.exception:
+            raise self.post_return_value.exception
+
+        if self.post_iterator:
+            self.post_return_value.value = next(self.post_iterator)
+
+        return self.post_return_value
 
     @check_response
     def put(self, path, *args, **kwargs):
-        self.called_with.append({"path": path, "kwargs": kwargs})
-        return self._return_value
+        self.put_called_with.append({"path": path, "kwargs": kwargs})
+
+        if self.put_return_value.exception:
+            raise self.put_return_value.exception
+
+        if self.put_iterator:
+            self.put_return_value.value = next(self.put_iterator)
+
+        return self.put_return_value
 
     @check_response
     def delete(self, path, *args, **kwargs):
-        self.called_with.append({"path": path, "kwargs": kwargs})
-        return self._return_value
+        self.delete_called_with.append({"path": path, "kwargs": kwargs})
+
+        if self.delete_return_value.exception:
+            raise self.delete_return_value.exception
+
+        if self.delete_iterator:
+            self.delete_return_value.value = next(self.delete_iterator)
+
+        return self.delete_return_value
 
 
-class PublicAPIV2Test(APIMock, PublicAPIV2):
+class PublicAPIV2Test(_APIMock, PublicAPIV2):
     def __init__(self):
         super().__init__()
 
 
-class IntegrationAPIV2Test(APIMock, IntegrationAPIV2):
+class IntegrationAPIV2Test(_APIMock, IntegrationAPIV2):
     def __init__(self):
         super().__init__()
 
--- tests/cli_tests/test_cli_animals.py
+++ tests/cli_tests/test_cli_animals.py
@@ -17,7 +17,7 @@ test_cli_user = CliUserTest()
 def test_cli_animals_get(print_mock):
 
     # test output for all animals and organisation_id
-    test_cli_user.integration_v2_api.mock_return_value(
+    test_cli_user.integration_v2_api.mock_get_return_value(
         ResponseMock([{"all": "animals"}], 200)
     )
 
@@ -32,7 +32,7 @@ def test_cli_animals_get(print_mock):
 
     # check if output was correctly printed to stdout
     assert print_mock.call_args[0][0] == '[{"all": "animals"}]'
-    assert test_cli_user.integration_v2_api.called_with[0] == {
+    assert test_cli_user.integration_v2_api.get_called_with[0] == {
         "kwargs": {"json": {"include_archived": False}},
         "path": "/organisations/parent_orga_id/animals",
     }
@@ -40,7 +40,7 @@ def test_cli_animals_get(print_mock):
     test_cli_user.integration_v2_api.reset_mock()
 
     # test output for multiple animals with id
-    test_cli_user.public_v2_api.mock_return_value(
+    test_cli_user.public_v2_api.mock_get_return_value(
         ResponseMock(
             [
                 {"animal_id": "1", "archived": True},
@@ -68,38 +68,40 @@ def test_cli_animals_get(print_mock):
         print_mock.call_args[0][0]
         == '[{"animal_id": "1", "archived": true}, {"animal_id": "3", "archived": true}]'
     )
-    assert test_cli_user.public_v2_api.called_with[0] == {
+    assert test_cli_user.public_v2_api.get_called_with[0] == {
         "kwargs": {"json": {"animal_ids": ["1", "2", "3", "4"]}},
         "path": "/animals/by_ids",
     }
     test_cli_user.public_v2_api.reset_mock()
 
-    # # test output for multiple animals with official id and archived
-    # animals_list = [
-    #     {"animal_id": "1", "archived": True},
-    #     {"animal_id": "2", "archived": False},
-    #     {"animal_id": "3", "archived": True},
-    #     {"animal_id": "4", "archived": True},
-    # ]
-    # test_cli_user.public_v2_api.mock_return_value(
-    #     ResponseMock(animals_list, 200)
-    # )
-    #
-    # # check for correct argument parsing
-    # namespace = args_parser(["animals", "get", "--ids", "1", "2", "3", "4", "--official-ids"])
-    # assert namespace.animals_sub_commands == "get"
-    # assert namespace.ids == ["1", "2", "3", "4"]
-    # assert namespace.official_ids is True
-    # assert namespace.organisation_id is None
-    # assert namespace.limit == 0
-    # assert namespace.archived is False
-    #
-    # assert len(test_cli_user.public_v2_api.called_with) == 4
-    # for idx, animal in enumerate(animals_list):
-    #     assert test_cli_user.public_v2_api.called_with[idx] == {
-    #         "kwargs": {"json": {}},
-    #     "path": f"/animals/by_official_id/{animal['animal_id']}/{test_cli_user.organisation_id}"
-    #     }
-    #
-    # # check if output was correctly printed to stdout
-    # assert print_mock.call_args[0][0] == '[{"animal_id": "2", "archived": false}]'
+    # test output for multiple animals with official id and archived
+    animals_list = [
+        {"animal_id": "1", "archived": True},
+        {"animal_id": "2", "archived": False},
+        {"animal_id": "3", "archived": True},
+        {"animal_id": "4", "archived": True},
+    ]
+    test_cli_user.public_v2_api.mock_get_return_value(
+        ResponseMock(animals_list, 200, iterator=True)
+    )
+
+    # check for correct argument parsing
+    namespace = args_parser(
+        ["animals", "get", "--ids", "1", "2", "3", "4", "--official-ids"]
+    )
+    assert namespace.animals_sub_commands == "get"
+    assert namespace.ids == ["1", "2", "3", "4"]
+    assert namespace.official_ids is True
+    assert namespace.organisation_id is None
+    assert namespace.limit == 0
+    assert namespace.archived is False
+
+    assert len(test_cli_user.public_v2_api.get_called_with) == 4
+    for idx, a in enumerate(animals_list):
+        assert test_cli_user.public_v2_api.get_called_with[idx] == {
+            "kwargs": {"json": {}},
+            "path": f"/animals/by_official_id/{test_cli_user.organisation_id}/{a['animal_id']}",
+        }
+
+    # check if output was correctly printed to stdout
+    assert print_mock.call_args[0][0] == '[{"animal_id": "2", "archived": false}]'
